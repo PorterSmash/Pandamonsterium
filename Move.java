2d7a033c43d1277ae7bc6cb85ca84fa93dcf74f2
PorterSmash <37052182+PorterSmash@users.noreply.github.com>
2018-09-12 20:30:10 -0400

    Update Move.java

--- Move.java
+++ Move.java
@@ -1,8 +1,10 @@
 
 public /*interface */class Move {
-	int attackPower;
-	int critChance;
+	
+	int attackPower; //some number between 1 and 10
+	int critChance; //represents a percentage
 	int hitChance;
+	int monsterApplied; //represents which monster the move is applied to. 1 if it is applied to the enemy, 0 if it is applied to itself
 	
 	public Move(int atkPwr, int crtChc, int htChc) {
 		attackPower = atkPwr;
