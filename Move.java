7d021101176d51ae91a0f3fdeedc0c89a02009f3
PorterSmash <37052182+PorterSmash@users.noreply.github.com>
2018-09-12 20:38:53 -0400

    Update Move.java

--- Move.java
+++ Move.java
@@ -1,15 +1,17 @@
 
 public /*interface */class Move {
 	
-	int attackPower; //some number between 1 and 10
+	int attackPower; //some number between -10 and 10, negative so that the monster can heal
 	int critChance; //represents a percentage
 	int hitChance;
-	int monsterApplied; //represents which monster the move is applied to. 1 if it is applied to the enemy, 0 if it is applied to itself
+	int moveTarget; //represents which monster the move is applied to. 1 if it is applied to the enemy, 0 if it is applied to itself
 	
-	public Move(int atkPwr, int crtChc, int htChc) {
+	
+	public Move(int atkPwr, int crtChc, int htChc, int mveTgt) {
 		attackPower = atkPwr;
 		critChance = crtChc;
 		hitChance = htChc;
+		moveTarget = mveTgt;
 	}
 }
 //shouldn't this be an interface?
