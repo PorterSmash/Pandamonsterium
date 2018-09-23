0dee2f740f768cf724729b470c3b45b32d705b09
kaukonej <36007247+kaukonej@users.noreply.github.com>
2018-09-23 13:02:10 -0400

    Delete MonsterInterface.java

--- MonsterInterface.java
+++
@@ -1,8 +0,0 @@
-package Release1;
-
-public interface MonsterInterface {
-	public void monsterFactory(String name);
-	public void levelUp(int statID);
-	public void decreaseHealth(int healthDecNum);
-	public Move getMove(int moveNum);
-}
