cd347f76ca44f063dfb2e832b8cca92e81eec5fb
Michelle Vu <31967665+vumic@users.noreply.github.com>
2018-12-05 12:32:20 -0500

    Delete MonsterInterface.java

--- src/release.Two/MonsterInterface.java
+++
@@ -1,8 +0,0 @@
-package release.Two;
-
-public interface MonsterInterface {
-	public void monsterFactory(String name);
-	public void levelUp(int statID);
-	public void decreaseHealth(int healthDecNum);
-	public Move getMove(int moveNum);
-}
